69d0082c8120b90009de6bb0cceb28b26d5f0220
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-04-03 15:30:32 +0600

    Add Bubblesort Sorting

--- Sorting/BubbleSort.java
+++ Sorting/BubbleSort.java
@@ -1,13 +1,24 @@
+import java.util.*;
+
 public class BubbleSort {
+    public static void printArray(int arr[]) {
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
         int arr[] = {5, 3, 8, 4, 2};
         for(int i=0;i<arr.length -1;i++){
             for(int j=0;j<arr.length-1-i;j++){
-                int temp = arr[j];
-                arr[j]=arr[j+1];
-                arr[j+1]=temp;
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j]=arr[j+1];
+                    arr[j+1]=temp;
+                }
             }
         }
-        System.out.println("Sorted Array: " + Arrays.toString(arr));
+        printArray(arr);
+        // System.out.println("Sorted Array: " + Arrays.toString(arr));
     }
 }
