b0630f35ede82844e6ed9df1fbf58ff9ea6ef8a2
sumiya-yasmin <sumiyayasmin665@gmail.com>
2025-04-09 23:46:59 +0600

    Add recursion

---
+++ Recursion/PrintNumberDescendingOrder.java
@@ -0,0 +1,15 @@
+public class PrintNumberDescendingOrder{
+    public static void printNumber(int n){
+        if(n==0){
+            return;
+        }
+       System.out.println(n);
+       printNumber(n-1);
+    }
+
+    public static void main(String[] args){
+        int n =6;
+        printNumber(n);
+    
+    }
+}
